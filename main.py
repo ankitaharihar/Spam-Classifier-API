ab2262fb8d3825cd3b3b52a73e650d637f039e89
Ankita <ankitaharihar81@gmail.com>
2026-06-09 14:25:22 +0530

    Add initial implementation of Spam Classifier API with model and vectorizer loading

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import uvicorn
+
+# Load model
+with open("spam_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+# Load vectorizer
+with open("vectorizer.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
+# Create FastAPI app
+app = FastAPI(title="Spam Classifier API")
+
+# Input schema
+class Message(BaseModel):
+    text: str
+
+# Home route
+@app.get("/")
+def home():
+    return {
+        "message": "Spam Classifier API Running 🚀"
+    }
+
+# Prediction route
+@app.post("/predict")
+def predict(message: Message):
+
+    # Convert text to vector
+    text_vector = vectorizer.transform([message.text])
+
+    # Predict
+    prediction = model.predict(text_vector)[0]
+
+    # Probability
+    probability = model.predict_proba(text_vector)[0].max()
+
+    # Result
+    result = "spam" if prediction == 1 else "not spam"
+
+    return {
+        "message": message.text,
+        "prediction": result,
+        "confidence": round(float(probability), 2)
+    }
+
+# Run app
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
